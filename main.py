02f68b8ca493255003840a3f9a98764feb62e0a6
lrnholanda <lrnholanda@gmail.com>
2021-03-10 02:15:34 +0000

    nova condições

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-age = 19
+age = 17
 if age >= 18:
   print("You are old enough to vote!")
+  print("Have yu registered to vote yet?")
+else:
+  print("Sorry, you ate too young to vote.")
+  print("Please register to vote as soon as you turn 18!")
